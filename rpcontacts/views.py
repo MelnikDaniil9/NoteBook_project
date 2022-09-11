369415817e9372e9bcd5171f7bf4c44469bfd957
Daniil <harddog556@gmail.com>
2022-09-11 14:32:35 +0300

    Added standart functional applications

--- rpcontacts/views.py
+++ rpcontacts/views.py
@@ -1,12 +1,19 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QAbstractItemView,
+    QDialog,
+    QDialogButtonBox,
+    QFormLayout,
     QHBoxLayout,
+    QLineEdit,
     QMainWindow,
+    QMessageBox,
     QPushButton,
     QTableView,
     QVBoxLayout,
     QWidget,
 )
+
 from rpcontacts.model import ContactsModel
 
 
@@ -28,8 +35,11 @@ class Window(QMainWindow):
         self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.table.resizeColumnsToContents()
         self.addButton = QPushButton("Добавить")
+        self.addButton.clicked.connect(self.openAddDialog)
         self.deleteButton = QPushButton("Удалить")
+        self.deleteButton.clicked.connect(self.deleteContact)
         self.clearAllButton = QPushButton("Очистить все")
+        self.clearAllButton.clicked.connect(self.clearContacts)
         layout = QVBoxLayout()
         layout.addWidget(self.addButton)
         layout.addWidget(self.deleteButton)
@@ -37,3 +47,86 @@ class Window(QMainWindow):
         layout.addWidget(self.clearAllButton)
         self.layout.addWidget(self.table)
         self.layout.addLayout(layout)
+
+    def openAddDialog(self):
+        dialog = AddDialog(self)
+        if dialog.exec() == QDialog.Accepted:
+            self.contactsModel.addContact(dialog.data)
+            self.table.resizeColumnsToContents()
+
+    def deleteContact(self):
+        row = self.table.currentIndex().row()
+        if row < 0:
+            return
+
+        messageBox = QMessageBox.warning(
+            self,
+            "Внимание!",
+            "Вы действительно хотите удалить контакт?",
+            QMessageBox.Ok | QMessageBox.Cancel,
+        )
+
+        if messageBox == QMessageBox.Ok:
+            self.contactsModel.deleteContact(row)
+
+    def clearContacts(self):
+        messageBox = QMessageBox.warning(
+            self,
+            "Внимание!",
+            "Вы хотите удалить ВСЕ контакты?",
+            QMessageBox.Ok | QMessageBox.Cancel,
+        )
+
+        if messageBox == QMessageBox.Ok:
+            self.contactsModel.clearContacts()
+
+
+class AddDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setWindowTitle("Add Contact")
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.data = None
+
+        self.setupUI()
+
+    def setupUI(self):
+        self.nameField = QLineEdit()
+        self.nameField.setObjectName("Name")
+        self.jobField = QLineEdit()
+        self.jobField.setObjectName("Job")
+        self.emailField = QLineEdit()
+        self.emailField.setObjectName("Email")
+        layout = QFormLayout()
+        layout.addRow("Name:", self.nameField)
+        layout.addRow("Job:", self.jobField)
+        layout.addRow("Email:", self.emailField)
+        self.layout.addLayout(layout)
+        self.buttonsBox = QDialogButtonBox(self)
+        self.buttonsBox.setOrientation(Qt.Horizontal)
+        self.buttonsBox.setStandardButtons(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        )
+        self.buttonsBox.accepted.connect(self.accept)
+        self.buttonsBox.rejected.connect(self.reject)
+        self.layout.addWidget(self.buttonsBox)
+
+    def accept(self):
+        self.data = []
+        for field in (self.nameField, self.jobField, self.emailField):
+            if not field.text():
+                QMessageBox.critical(
+                    self,
+                    "Error!",
+                    f"You must provide a contact's {field.objectName()}",
+                )
+                self.data = None
+                return
+
+            self.data.append(field.text())
+
+        if not self.data:
+            return
+
+        super().accept()
